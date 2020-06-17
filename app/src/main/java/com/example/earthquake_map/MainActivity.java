4992114fad19b6e797a15dd35c0951923618f68c
jay23cee <jayceexxiii@gmail.com>
2020-06-16 21:47:15 -0700

    added a textview. Displays Earthquake geojson.

--- app/src/main/java/com/example/earthquake_map/MainActivity.java
+++ app/src/main/java/com/example/earthquake_map/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -16,8 +19,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity  extends AppCompatActivity
-        implements OnMapReadyCallback {
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity  extends AppCompatActivity implements OnMapReadyCallback {
+    private TextView earthquake_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,42 @@ public class MainActivity  extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        earthquake_result = findViewById(R.id.earthquake_API);
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-02";
+
+
+            Request request = new Request.Builder()
+                    .get()
+                    .url(url)
+                    .build();
+
+
+          client.newCall(request).enqueue(new Callback() {
+              @Override
+              public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                  e.printStackTrace();
+              }
+
+              @Override
+              public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    final String myResponse = response.body().string();
+                     final String myResponse1 = response.toString();
+
+                  //getting whole json string
+
+
+                  MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            earthquake_result.setText(myResponse);
+                        }
+                    });
+              }
+          });
     }
 
 
@@ -37,5 +93,14 @@ public class MainActivity  extends AppCompatActivity
         googleMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
+
+
+
+
+
+
     }
+
+
+
 }
